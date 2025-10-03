710f9f6b944bbfab0b6232a400e6e8adec7daa26
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-10-03 14:09:37 +0300

    Базовый пинг дискорд бота

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 docker build . -t svc-discord:latest
 docker run --rm svc-discord:latest
 ```
+
+Для конфигурации используются вот такие ENV переменные:
+- DISCORD_BOT_TOKEN - токен бота в дискорд
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	log.Println("Запуск svc-discord..")
+	token := "Bot " + getEnv("DISCORD_BOT_TOKEN", "nil")
+
+	dg, err := discordgo.New(token)
+	if err != nil {
+		log.Fatalln("Неверный токен:", err)
+	}
+
+	user, err := dg.User("@me")
+	if err != nil {
+		log.Fatalln("Ошибка при проверке токена:", err)
+	}
+
+	log.Printf("Токен рабочий! Бот: %s#%s\n", user.Username, user.Discriminator)
 }
